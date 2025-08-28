707b67fec71e780c7356a467757209b91cfbad7a
kylegortych <99693659+kylegortych@users.noreply.github.com>
2025-08-27 20:41:31 -0400

    updated shell.nix to include qemu tools

--- shell.nix
+++ shell.nix
@@ -3,47 +3,18 @@
 (pkgs.buildFHSEnv {
   name = "buildroot-env";
   multiPkgs = pkgs: with pkgs; [
-    bash
-    coreutils
-    gcc
-    gnumake
-    binutils
-    bison
-    flex
-    bc
-    perl
-    unzip
-    cpio
-    rsync
-    which
-    file
-    python3
-    git
-    wget
-    patch
-    ncurses.dev
-    findutils
-    util-linux
-    gawk
-    gnutar
-    zlib
-    glibc
-    diffutils
-    gettext
-    xz
-    gzip
-    bzip2
-    lzop
-    lz4
-    zstd
-    pkg-config
-    autoconf
-    automake
-    libtool
-    texinfo
-    openssl.dev
-    curl
-    subversion
+    # Tools for Buildroot
+    bash coreutils gcc gnumake binutils bison flex bc
+    perl unzip cpio rsync which file python3 git wget patch
+    ncurses.dev findutils util-linux gawk gnutar zlib glibc
+    diffutils gettext xz gzip bzip2 lzop lz4 zstd
+    pkg-config autoconf automake libtool texinfo
+    openssl.dev curl subversion
+    
+    # Tools for QEMU
+    qemu
+    qemu_kvm
+    qemu-utils
   ];
 
   runScript = "bash";
